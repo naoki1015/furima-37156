519f306e5b6ffc0e69edc145b36e3fdc48c983dd
naoki1015 <naokijudy06290620@gmail.com>
2022-03-06 03:56:35 +0900

    userモデルのvalidation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+ has_many :items
+ has_many :purchase_records
+
+
+ with_options presence: true do
+  validates :nickname
+  validates :email
+  validates :encrypted_password
+  validates :last_name
+  validates :first_name
+  validates :last_name_kana
+  validates :first_name_kana
+  validates :birthday
+ end
+ 
 end
