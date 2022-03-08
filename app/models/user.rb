08d4270883cf223dc60f50c27a2490759c6b7ec6
naoki1015 <naokijudy06290620@gmail.com>
2022-03-09 01:11:02 +0900

    ユーザー管理機能rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,28 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
- has_many :items
- has_many :purchase_records
+  has_many :items
+  has_many :purchase_records
 
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
 
- with_options presence: true do
-  validates :nickname
-  validates :birthday
- end
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 
- 
- PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-validates_format_of :password, with: PASSWORD_REGEX
-  
- with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
-  validates :last_name
-  validates :first_name
- end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+    validates :last_name
+    validates :first_name
+  end
 
- with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
-  validates :last_name_kana
-  validates :first_name_kana
- end
- 
- 
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
 end
