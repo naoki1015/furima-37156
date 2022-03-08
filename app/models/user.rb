8d0df124fcfae6af181ce808fb33de730866c506
naoki1015 <naokijudy06290620@gmail.com>
2022-03-09 01:04:43 +0900

    userモデルのvalidation修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,22 @@ class User < ApplicationRecord
 
  with_options presence: true do
   validates :nickname
-  validates :email
-  validates :encrypted_password
-  validates :last_name
-  validates :first_name
-  validates :last_name_kana
-  validates :first_name_kana
   validates :birthday
  end
+
+ 
+ PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+validates_format_of :password, with: PASSWORD_REGEX
+  
+ with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+  validates :last_name
+  validates :first_name
+ end
+
+ with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+  validates :last_name_kana
+  validates :first_name_kana
+ end
+ 
  
 end
