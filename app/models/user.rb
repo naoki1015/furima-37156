f880637a2ca2eefb0afd3fa343b99302bf65d825
naoki1015 <naokijudy06290620@gmail.com>
2022-03-11 15:05:45 +0900

    userモデルvalidation修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :purchase_records
+  # has_many :items
+  # has_many :purchase_records
 
   with_options presence: true do
     validates :nickname
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
     validates :last_name
     validates :first_name
   end
