0aab23229f46e58e24e00f260f21d094c983c167
naoki1015 <naokijudy06290620@gmail.com>
2022-03-13 02:50:15 +0900

    Active_hashのモデル、バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :deadline
+  belongs_to :prefecture
+  belongs_to :shopping_charge
+  belongs_to :status
+
+  validates :name, :explanation, :price, presence: true
+
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :deadline_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shopping_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
