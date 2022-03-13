064e347da23246919982a7b27b1338b47b0965a2
naoki1015 <naokijudy06290620@gmail.com>
2022-03-14 00:03:13 +0900

    ログイン状態のみ商品出品ページへ以降できる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,24 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
+
   def index
   end
+
 
   def new
     @item = Item.new
   end
 
   def create
+    Item.create(item_params)
+    redirect_to root_path
   end
-  
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :shopping_charge_id, :prefecture_id, :deadline_id, :status_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
