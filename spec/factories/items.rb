13e3c2f0ccf0e2fcaceab2e3fa4db9950c3419fc
naoki1015 <naokijudy06290620@gmail.com>
2022-03-18 20:44:48 +0900

    FactoryBotの作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name                { 'sample' }
+    explanation         {'sample_explanation'}
+    category_id         {2}
+    status_id           {2}
+    shopping_charge_id  {2}
+    prefecture_id       {2}
+    deadline_id         {2}
+    price               {3000}
+   
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
