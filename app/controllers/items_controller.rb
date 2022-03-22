f558a2e424097cc2debb4c813022eacdcc015905
naoki1015 <naokijudy06290620@gmail.com>
2022-03-22 19:17:52 +0900

    item_controllerの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,8 +19,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if current_user.id == @item.user_id
     @item.destroy
     redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   def show
