f1ed86e83219d618a2b9e83df6e4563b5535ba3a
naoki1015 <naokijudy06290620@gmail.com>
2022-03-31 13:16:44 +0900

    itemモデルアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
-  # has_one    :purchase_record
+  has_one    :purchase_record
   belongs_to :category
   belongs_to :deadline
   belongs_to :prefecture
