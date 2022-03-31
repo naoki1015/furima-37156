6d0e8337a967e381646b612d1a5fc6bfc75770be
naoki1015 <naokijudy06290620@gmail.com>
2022-03-31 13:20:47 +0900

    ルーティングのネスト記述

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "items#index"
   
-  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    resources :purchase_record, only: [:index, :create]
+  end
+
 end
