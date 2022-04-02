f7e75f40a6579df4035654a80f88593307d7922e
naoki1015 <naokijudy06290620@gmail.com>
2022-04-02 14:54:11 +0900

    purchase_records_controllerの作成

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,9 +1,26 @@
 class PurchaseRecordsController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
+    @purchase_record_address = PurchaseRecordAddress.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @purchase_record_address = PurchaseRecordAddress.new(purchase_record_params)
+    if @purchase_record_address.valid?
+      @purchase_record_address.save
+      redirect_to root_path
+
+    else
+      render :index
+    end
+
   end
-  
+
+  private
+
+  def purchase_record_params
+    params.require(:purchase_record_address).permit(:post_code, :prefecture_id, :municipality, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
 end
