baf6b5410767fcae45adc7e02c459395be65347b
naoki1015 <naokijudy06290620@gmail.com>
2022-04-02 14:52:40 +0900

    Formオブジェクトの修正

--- app/models/purchase_record_address.rb
+++ app/models/purchase_record_address.rb
@@ -5,17 +5,16 @@ class PurchaseRecordAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :post_code
-    validates :prefecture_id
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :prefecture_id, numericality: { other_than: 1}
     validates :municipality
     validates :house_number
-    validates :phone_number
-    validates :purchase_record_id
+    validates :phone_number, format: {with: /\A\d{10,11}\z/}
   end
 
 
   def save
-    purchase_record = Purchase_record.create(user_id: user_id, item_id: item_id)
+    purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
 
     Address.create(post_code: post_code, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number, building: building, phone_number: phone_number, purchase_record_id: purchase_record.id)
   end
