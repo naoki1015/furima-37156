f14f444d3669d4ea767fda5b1a82ac7067013411
naoki1015 <naokijudy06290620@gmail.com>
2022-04-04 17:47:53 +0900

    purchase_recordsコントローラーの修正

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,8 +1,14 @@
 class PurchaseRecordsController < ApplicationController
 
+  before_action :authenticate_user!, only: :index
+
   def index
     @item = Item.find(params[:item_id])
     @purchase_record_address = PurchaseRecordAddress.new
+
+    if current_user.id == @item.user_id || @item.purchase_record.present?
+      redirect_to root_path
+    end
   end
 
   def create
