347b490859127a666af5f614138fd056c975ac28
naoki1015 <naokijudy06290620@gmail.com>
2022-04-05 09:17:57 +0900

    purchase_recordコントローラーの修正2

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,32 +1,43 @@
 class PurchaseRecordsController < ApplicationController
-
+  before_action :set_item, only: [:index, :create]
   before_action :authenticate_user!, only: :index
 
   def index
-    @item = Item.find(params[:item_id])
     @purchase_record_address = PurchaseRecordAddress.new
 
-    if current_user.id == @item.user_id || @item.purchase_record.present?
-      redirect_to root_path
-    end
+    redirect_to root_path if current_user.id == @item.user_id || @item.purchase_record.present?
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchase_record_address = PurchaseRecordAddress.new(purchase_record_params)
     if @purchase_record_address.valid?
+      pay_item
       @purchase_record_address.save
       redirect_to root_path
 
     else
       render :index
     end
-
   end
 
   private
 
   def purchase_record_params
-    params.require(:purchase_record_address).permit(:post_code, :prefecture_id, :municipality, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchase_record_address).permit(:post_code, :prefecture_id, :municipality, :house_number, :building, :phone_number).merge(
+      user_id: current_user.id, item_id: @item.id, token: params[:token]
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: @purchase_record_address.token,
+      currency: 'jpy'
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
