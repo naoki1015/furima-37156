b01cc7d8d7db5685eb54e74665275cf2a0638581
naoki1015 <naokijudy06290620@gmail.com>
2022-04-05 09:16:44 +0900

    rubocopの実行

--- app/models/purchase_record_address.rb
+++ app/models/purchase_record_address.rb
@@ -1,21 +1,23 @@
 class PurchaseRecordAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :house_number, :building, :phone_number, :purchase_record_id ,:token
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :house_number, :building, :phone_number,
+                :purchase_record_id, :token
 
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
-    validates :prefecture_id, numericality: { other_than: 1}
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 1 }
     validates :municipality
     validates :house_number
-    validates :phone_number, format: {with: /\A\d{10,11}\z/}
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :token
   end
-
 
   def save
     purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
 
-    Address.create(post_code: post_code, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number, building: building, phone_number: phone_number, purchase_record_id: purchase_record.id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number,
+                   building: building, phone_number: phone_number, purchase_record_id: purchase_record.id)
   end
 end
